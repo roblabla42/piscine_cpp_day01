1ecace6a74d2df06e6c36ccf5ee3da12526681ef
roblabla <robinlambertz+dev@gmail.com>
2015-06-16 10:19:46 +0000

    Add destructor to ex00

--- ex00/Pony.cpp
+++ ex00/Pony.cpp
@@ -6,7 +6,7 @@
 /*   By: roblabla </var/spool/mail/roblabla>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/11 14:55:47 by roblabla          #+#    #+#             */
-/*   Updated: 2015/06/11 15:06:12 by roblabla         ###   ########.fr       */
+/*   Updated: 2015/06/16 10:16:45 by roblabla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 Pony::Pony(std::string name) : name(name)
 {
+}
+
+Pony::~Pony()
+{
+	std::cout	<< "A pony just died. Weep a bit : http://goo.gl/wUknjP"
+				<< std::endl;
 }
 
 void Pony::talk() {
--- ex00/Pony.hpp
+++ ex00/Pony.hpp
@@ -6,7 +6,7 @@
 /*   By: roblabla </var/spool/mail/roblabla>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/11 14:58:06 by roblabla          #+#    #+#             */
-/*   Updated: 2015/06/11 15:05:07 by roblabla         ###   ########.fr       */
+/*   Updated: 2015/06/16 10:14:44 by roblabla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ class Pony {
 	std::string	name;
 	Pony(std::string name);
 public:
+	~Pony();
 	void talk();
 	static void ponyOnStack();
 	static void ponyOnHeap();
