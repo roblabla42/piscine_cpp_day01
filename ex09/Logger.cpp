91fa1d4fde215eeed1478ee011cad6162986e771
roblabla <robinlambertz+dev@gmail.com>
2015-06-17 09:28:18 +0000

    Still no right to use std::map

--- ex09/Logger.cpp
+++ ex09/Logger.cpp
@@ -6,7 +6,7 @@
 /*   By: roblabla </var/spool/mail/roblabla>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/12 15:19:33 by roblabla          #+#    #+#             */
-/*   Updated: 2015/06/12 16:18:07 by roblabla         ###   ########.fr       */
+/*   Updated: 2015/06/17 09:18:16 by roblabla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,14 @@ std::string	Logger::makeLogEntry(std::string str)
 
 void		Logger::log(std::string const & dest, std::string const & msg)
 {
-	std::map<std::string, void (Logger::*)(std::string)> fns;
-	fns["console"] = &Logger::logToConsole;
-	fns["file"] = &Logger::logToFile;
-	if (fns.count(dest))
-		(this->*fns[dest])(msg);
+	std::string keys[] = { "console", "file" };
+	void (Logger::*values[])(std::string) = {
+		&Logger::logToConsole,
+		&Logger::logToFile
+	};
+	size_t i = 0;
+	while (i < 2 && keys[i] != dest)
+		i++;
+	if (i < 2)
+		(this->*values[i])(msg);
 }
