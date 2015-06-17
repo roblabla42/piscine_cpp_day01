4a5a6a896c7ca892d1012de46b35c6a66ca64a7c
roblabla <robinlambertz+dev@gmail.com>
2015-06-17 09:16:50 +0000

    Can't use std::map

--- ex08/Human.cpp
+++ ex08/Human.cpp
@@ -6,13 +6,12 @@
 /*   By: roblabla </var/spool/mail/roblabla>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/12 13:17:28 by roblabla          #+#    #+#             */
-/*   Updated: 2015/06/12 16:18:27 by roblabla         ###   ########.fr       */
+/*   Updated: 2015/06/17 09:18:32 by roblabla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Human.hpp"
 #include <iostream>
-#include <map>
 
 void	Human::meleeAttack(std::string const & target)
 {
@@ -31,10 +30,15 @@ void	Human::intimidatingShout(std::string const & target)
 
 void	Human::action(std::string const & action_name, std::string const & target)
 {
-	std::map<std::string, void (Human::*)(std::string const &)> fns;
-	fns["melee"] = &Human::meleeAttack;
-	fns["ranged"] = &Human::rangedAttack;
-	fns["shout"] = &Human::intimidatingShout;
-	if (fns.count(action_name))
-		(this->*fns[action_name])(target);
+	std::string	keys[] = { "melee", "ranged", "shout" };
+	void (Human::*values[])(std::string const &) = { 
+		&Human::meleeAttack,
+		&Human::rangedAttack,
+		&Human::intimidatingShout
+	};
+	size_t	i = 0;
+	while (i < 3 && keys[i] != action_name)
+		i++;
+	if (i < 3)
+		(this->*values[i])(target);
 }
